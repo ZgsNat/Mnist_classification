03f5f7b40169837d2138a72e8849c382038d7589
ZgsNat <nguyenthaithinh9876@gmail.com>
2025-10-21 23:29:16 +0700

    upgrade model with more block layer, training with data augmentation

--- training_mnist.py
+++ training_mnist.py
@@ -1,27 +1,53 @@
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
 
-y_train = tf.keras.utils.to_categorical(y_train, 10)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
+
+# Data augmentation
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+datagen.fit(x_train)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
+    # Block 1
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(28,28,1)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Dropout(0.25),
 
-    tf.keras.layers.Dropout(0.2),
+    # Block 2
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Dropout(0.25),
+
+    # Block 3
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Dense(10, activation='softmax')
+
 ])
 
 model.summary()
@@ -30,12 +56,23 @@ model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-history = model.fit(x_train, y_train,
-                    batch_size=128,
-                    epochs=10,
-                    validation_data=(x_test, y_test))
+# Callbacks
+callbacks = [
+    EarlyStopping(patience=5, restore_best_weights=True),
+    ReduceLROnPlateau(factor=0.5, patience=2, verbose=1)
+]
+
+# Training with data augmentation
+history = model.fit(
+    datagen.flow(x_train, y_train, batch_size=128),
+    epochs=30,
+    validation_data=(x_test, y_test),
+    callbacks=callbacks,
+    verbose=1
+)
 
 test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
 print('\nTest accuracy:', test_acc)
 
-model.save('mnist_cnn_model.h5')
+# model.save('mnist_cnn_model.h5')
+# model.save('mnist_cnn_model_2.h5')
